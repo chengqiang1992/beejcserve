363e6016d98b8019c054a8968d6fb17afa4aa41f
chengqiang <xpluto1992@gmail.com>
2017-01-10 17:00:00 +0800

    操作文件

--- BeginningPython/Unit_7/InputandOutput.py
+++ BeginningPython/Unit_7/InputandOutput.py
@@ -76,3 +76,31 @@ print('The story of {0}, {1}, and {other}.'.format('Bill', 'Manfred', other='Geo
 
 print('The value of PI is approximately %5.3f.' % math.pi)
 
+"""
+7.2 Reading and Writing Files
+open() returns a file object, and is most commonly used with two arguments: open(filename, mode).
+
+The first argument is a string containing the filename. The second argument is another string containing
+a few characters describing the way in which the file will be used. mode can be 'r' when the file will only
+be read, 'w' for only writing (an existing file with the same name will be erased), and 'a' opens the file
+for appending; any data written to the file is automatically added to the end. 'r+' opens the file for both
+reading and writing. the mode argument is optional; 'r' will be assumed if it's omitted.
+
+Normally, files are opened in text mode, that means, you read and write string from and to the file, which
+are encoded in a specific encoding. if encoding is not specified, the default is platform dependent. 'b' appended
+to the mode opens the file in binary mode: now the data is read and written in the form of bytes objects. This mode
+should be used for all files that don't contain text.
+
+In text mode, the default when reading is to convert platform-specific line endings (\n on unix, \r\n on Windows)
+to just \n. When writing in text mode, the default is to convert occurrences of \n back to platform-specific
+line endings. this behind-the-scenes modification to file data is fine for text files, but will corrupt binary data
+like that in JPEG or EXG files. Be very careful to use binary mode when reading and writing such files.
+ """
+
+f = open('沁园春-长沙.txt', 'w', encoding="utf-8")
+print(f)
+
+i = open('夏目.jpg', 'rb+')
+print(i)
+
+
