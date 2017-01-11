471dbd3b460dc84ef01f80594b71ca99ef729170
chengqiang <xpluto1992@gmail.com>
2017-01-11 10:26:18 +0800

    1： 类

---
+++ BeginningPython/Unit_9/Classes.py
@@ -0,0 +1,20 @@
+"""
+9. Classes
+    9.1 A Word About Names and Objects
+    9.2 Python Scopes and Namespaces
+        9.2.1 Scopes and Namespaces Example
+    9.3 A First Look at Classes
+        9.3.1 Class Definition Syntax
+        9.3.2 Class Objects
+        9.3.3 Instance Objects
+        9.3.4 Method Objects
+        9.3.5 Class and Instance Variables
+    9.4 Random Remarks
+    9.5 Inheritance
+        9.5.1 Multiple Inheritance
+    9.6 Private Variables
+    9.7 Odds and Ends
+    9.8 Iterators
+    9.9 Generators
+    9.10 Generator Expressions
+"""
