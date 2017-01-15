7b118280734d58b6b2c2552a105f5743b8d26bc8
chengqiang <xpluto1992@gmail.com>
2017-01-15 23:58:46 +0800

    add some detail about classmethod in the class

--- ThePythonStandardLibrary/Unit_2/test.py
+++ ThePythonStandardLibrary/Unit_2/test.py
@@ -11,5 +11,29 @@ class Data_test(object):
     def out_data(self):
         print("year:" + self.year + " month:" + self.month + " day:" + self.day)
 
-t = Data_test(2016, 8, 1)
+t = Data_test('2016', '8', '1')
 t.out_data()
+
+
+class Data_tets2(object):
+    day = 0
+    month = 0
+    year = 0
+
+    def __init__(self, year=0, month=0, day=0):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def out_date(self):
+        print("year:" + self.year + " month:" + self.month + " day:" + self.day)
+
+    @classmethod
+    def get_date(cls, data_as_string):
+        # 这里的第一个参数是cls，表示调用当前的类名
+        year, month, day = map(int, data_as_string.split('-'))
+        datel = cls(year, month, day)
+        return datel
+
+r = Data_tets2.get_date("2016-8-6")
+r.out_date()
