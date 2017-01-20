505a45bb01281c9c8d32fca27c6db933fc027407
chengqiang <xpluto1992@gmail.com>
2017-01-20 17:06:43 +0800

    1：python standard library Iterator Types Sequence Type

--- ThePythonStandardLibrary/Unit_4/Built-inTypes.py
+++ ThePythonStandardLibrary/Unit_4/Built-inTypes.py
@@ -116,4 +116,138 @@ Notes
     4. float also accepts the strings “nan” and “inf” with an optional prefix “+” or “-” for Not a Number (NaN)
         and positive or negative infinity.
     5. Python defines pow(0, 0) and 0 ** 0 to be 1, as is common for programming languages.
+
+
+    4.4.1 Bitwise Operations on Integer Types
+    Bitwise operations only make sense for integers. Negative numbers are treated as their 2's complement value (this
+    assumes that there are enough bits so that no overflow occurs during the operation).
+
+    The priorities of the binary are all lower than the numeric operations and higher than the comparisons; the unary
+    operation ~ has the same priority as the other unmeric operation (+ and -)
+
+    This table lists the bitwise operations sorted in ascending priority
+
+            Operations          Result                          Notes
+              x | y         bitwise or of x and y
+              x ~ y         bitwise exclusive or of x and y
+              x & y         bitwise and of x and y
+              x << n        x shifted left by n bits            (1)(2)
+              x >> n        x shifted right by n bits           (1)(3)
+              ~x            The bits of x inverted
+    Notes:
+        1. Negative shift counts are illegal and cause a ValueError to be raised.
+        2. A left shift by n bits is equivalent to multiplication by pow(2, n) without overflow check.
+        3. A right shift by n bits is equivalent to division by pow(2, n) without overflow check.
+
+    4.4.2. Additional Methods on Integer Types
+    The int type implements the numbers. Integral abstract base class. In addition, it provides a few more methods:
+
+    int.bit_length()
+        return the number of bits necessary to represent an integer in binary, excluding the sign and leading zeros:
+            -0b100101
+            6
+            0b0
+            0
+            0b11111001110010111
+            17
+        More precisely, if x is nonzero, then x.bit_length() is the unique positive integer k such that 2**(k-1) <=
+        abs(x) < 2**k.
+
+
+4.5. Iterator Types
+ Python supports a concept of iteration over containers. This is implemented using two distinct methods; these are used
+ to allow user-defined classes to support iteration. Sequences, described below in more detail, always support the
+ iteration methods.
+
+ One method needs to be defined for container objects to provide iteration support:
+    container.__iter__()
+        Return an iterator object. The object is required to support the iterator protocol described below. If a
+        container supports different types of iteration, additional methods can be provided to specifically request
+        iterators for those iteration types. (An example of an object supporting multiple forms of iteration would be a
+        tree structure which supports both breadth first and depth-first traversal.) This method corresponds to the
+        tp_iter slot of the type structure for Python objects in the Python/C API.
+
+The iterator objects themselves are required to support the following two methods, which together form the iterator
+protocol:
+    iterator.__iter__()
+        Return the iterator object itself. This is required to allow both containers and iterators to be sued with the
+        for and in statements. This method corresponds to the tp_iter slot of the type structure for Python object in
+        the Python/C API.
+    iterator.__next__()
+        Return the next item from the container. If there are no further items, raise the StopIterator exception. This
+        method corresponds to the tp_iternext slot of the type stricture for Python objects in the Python/C API.
+
+Python defines several iterator objects to support iteration over general and specific sequence types, dictionaries, and
+other more specialized forms. The specific types are not important beyond their implementation of the iterator protocol.
+
+Once an iterator's __next__() method raises StopIteration, it must continue to do so on subsequent calls.
+Implementations that do not obey this property are deemed broken.
+
+    4.5.1. Generator Types
+
+    Python's generator provide a convenient way to implement the iterator protocol. If a container object's __iter__()
+    method is implemented as a generator, it will automatically return an iterator object (technically, a generator
+    object) supplying the __iter__() and __next__() methods. More information about generator can be found in the
+    documentation for the yield expression
+
+4.6. Sequence Type -- list, tuple, range
+There are three basic sequence types: list, tuples, and range objects. Additional sequence types tailored for processing
+of binary data and text strings are described in dedicated sections.
+
+    4.6.1. Common Sequence Operations
+    The operations in the following table are supported by most sequence types, both mutable and immutable. The
+    collection.abc.Sequence ABC is provided to make it easier to correctly implement these operations on custom sequence
+    types.
+
+    This table lists the sequence operations sorted in ascending priority. In the table, s and t are sequences of the
+    same type, n, i, j and k are integers and x is an arbitray boject that meets any type and value restrictions imposed
+    by s.
+
+    The in and not in operations have the same priorities as the comparison operations. The + (concatenation) and * (
+    repetition) operations have the same priority as the corresponding numeric operations.
+
+            Operation                   Result                                              Notes
+            x in s                      True if an item of s is equal to x,else False       (1)
+            x not in s                  False if an item of s is equal ro x , else True     (1)
+            s + t                       the concatenation of s and t                        (6)(7)
+            s * n or n * s              equivalent to adding s to itself n times            (2)(7)
+            s[i]                        ith item of s, origin 0                             (3)
+            s[i:j]                      slice of s from i to j                              (3)(4)
+            s[i:j:k]                    slice of s from i to j with step k                  (3)(5)
+            len(s)                      length of s
+            min(s)                      smallest item of s
+            max(s)                      largest item of s
+            s.index(x[, i[, j]])        index of the first occurrence of x in s (at or after    (8)
+                                        index i and before index j)
+            s.count(x)                  total number of occurrences of x in s
+
+    Sequence of the same type also support comparisons. In particular, tuples and lists are compared lexicographically
+    by comparing corresponding elements. This means that to compare, every element must compare equal and the two
+    sequences must be of the same type and have the same length. (For full details see Comparisons in the language
+    reference)
+
+    Notes:
+        1. While the in and not in operations are used only for simple containment testing in the general case, some
+        specialised sequences (such as str, bytes and bytearray)  also use them for subsequence testing:
+                print("gg" in "eggs")           # True
+        2. Values of n less than 0 are treated as 0 (which yields an empty sequence of the same type as s). Note that
+        items in the sequence s are not copied; they are referenced multiple times. This often haunts new Python
+        programmers:
+        consider:
+                lists = [[]] * 3
+                print(lists)            # [[], [], []]
+                lists[0].append(3)
+                print(lists)            # [[3], [3], [3]]
+        3. if i or j is negative, the index is relative to the end of sequence s: len(s) + i or len(s) + j is substituted.
+        But note that -0 is still 0
+        4. The slice of s from i to j is defined as the sequence of items with index k such that i <= k < j. If i or j
+        is greater than len(s), use len(s). If it is omitted or None, use 0. If j is omitted or None, use len(s). If i
+        is greater than or equal to j, the slice is empty.
+        5. The slice of s from i to j with step k is defined as the sequence of items with index x = i + n*k such that
+        0 <= n < (j-i)/k. In other words, the indices are i, i+k, i+2*k, i+3*k and so on, stopping when j is reached (
+        but never including j). When k is positive, i and j are reduced to len(s) if they are greater. When k is
+        negative, i and j are deduced to len(s) - 1 if they are greater. If i or j are omitted or None, they become
+        "end" values (which end depends on the sign of k). Note, K cannot be zero. If k is None, it is treated like 1.
+
+
 """
--- ThePythonStandardLibrary/Unit_4/test.py
+++ ThePythonStandardLibrary/Unit_4/test.py
@@ -4,3 +4,25 @@ print(1 // 2)
 
 print(sys.float_info)
 print(sys.float_info.max)
+
+n = -37
+bin(n)
+print(bin(n))
+print(n.bit_length())
+
+m = 0
+bin(m)
+print(bin(m))
+print(m.bit_length())
+
+x = 127895
+bin(x)
+print(bin(x))
+print(x.bit_length())
+
+print("gg" in "eggs")
+
+lists = [[]] * 3
+print(lists)
+lists[0].append(3)
+print(lists)
