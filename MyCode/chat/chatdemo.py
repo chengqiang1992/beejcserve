07a26f9517e9ea3c16d6dfa8288f27e732257388
chengqiang <xpluto1992@gmail.com>
2017-03-01 23:38:12 +0800

    chat project

--- MyCode/chat/chatdemo.py
+++ MyCode/chat/chatdemo.py
@@ -27,6 +27,88 @@ from tornado.concurrent import Future
 from tornado import gen
 from tornado.options import define, options, parse_command_line
 
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=False, help="run in debug mode")
+
+
+class MessageBuffer(object):
+    def __init__(self):
+        self.writers = set()
+        self.cache = []
+        self.cache_size = 200
+
+    def wait_for_message(self, cursor=None):
+        # Constructor a Future to return to our caller. This allows
+        # wait_for_messages to be yielded from a coroutine even though
+        # it is not a coroutine itself. We will set the result of the
+        # Future when result are available
+        result_future = Future()
+        if cursor:
+            new_count = 0
+            for msg in reversed(self.cache):
+                if msg["id"] == cursor:
+                    break
+                new_count += 1
+            if new_count:
+                result_future.set_result(self.cache[-new_count:])
+                return result_future
+        self.waiters.add(result_future)
+        return result_future
+
+    def cancel_wait(self, future):
+        self.waiters.remove(future)
+        # Set an empty result to unblock any coroutines waiting
+        future.set_result([])
+
+    def new_messages(self, message):
+        logging.info("Sending new message to %r listeners", len(self.waiters))
+        for future in self.waiters:
+            future.set_result(message)
+        self.waiters = set()
+        self.cache.extend(message)
+        if len(self.cache) > self.cache_size:
+            self.cache = self.cache[-self.cache_size:]
+
+
+global_message_buffer = MessageBuffer()
+
+
+class MainHandler(tornado.web.RedirectHandler):
+    def get(self):
+        self.render("index.html", messages=global_message_buffer.cache)
+
+
+class MessageNewHandler(tornado.web.RequestHandler):
+    def post(self):
+        message = {
+            "id": str(uuid.uuid4()),
+            "body": self.get_argument("body")
+        }
+        # to_basestring is necessary for Python 3's json encode,
+        # which doesn't accept byte string
+        message["html"] = tornado.escape.to_basestring(self.render_string("message.html", message=message))
+        if self.get_argument("next", None):
+            self.redirect(self.get_argument("next"))
+        else:
+            self.write(message)
+        global_message_buffer.new_messages([message])
+
+
+class MessageUpdateHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def post(self):
+        cursor = self.get_argument("cursor", None)
+        # Save the future returned by wait_for_messages so we can cancel
+        # it in wait_for_message
+        self.future = global_message_buffer.wait_for_message(cursor=cursor)
+        message = yield self.future
+        if self.request.connection.stream.closed():
+            return
+        self.write(dict(message=message))
+
+    def on_connection_close(self):
+        global_message_buffer.cancel_wait(self.future)
+
 
 def main():
     parse_command_line()
---
+++ ThePythonStandardLibrary/Unit_16/Miscellaneousoperatingsysteminterfaces.py
@@ -0,0 +1,8 @@
+"""
+16.1. os --- Miscellaneous operating system interfaces
+
+    This module provides a portable way of using operating system dependent functionality. If you just want to read or
+    write a file see open(), if you want to manipulate, see the os.path module, and if you want to read all the lines in
+    all the files on the command line see the fileinput module. For creating temporary files and directories see the
+    tempfile module, and for hight-level file and directory handling see the shutil module.
+"""
